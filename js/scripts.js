856c5181a600694a827f14a18004101f04fc78a5
Julia Dickey and Katarina Tuttle <katarina@>
2016-06-01 09:43:07 -0700

    Working madlibs

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+  $("#blanks form").submit(function(event) {
+    var person1Input = $("input#person1").val();
+    var person2Input = $("input#person2").val();
+    var placeInput = $("input#place").val();
+    var transportationInput = $("input#transportation").val();
+    var exclamationInput = $("input#exclamation").val();
+    var adjectiveInput = $("input#adjective").val();
+    var animalInput = $("input#animal").val();
+
+    $(".person1").text(person1Input);
+    $(".person2").text(person2Input);
+    $(".place").text(placeInput);
+    $(".transportation").text(transportationInput);
+    $(".exclamation").text(exclamationInput);
+    $(".adjective").text(adjectiveInput);
+    $(".animal").text(animalInput);
+
+    $("#story").show();
+
+    event.preventDefault();
+
+  });
+});
