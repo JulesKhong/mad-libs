ec34cd94f31b746ca2b5ea2cdaa61700ad6a2b7c
Halle Williams and Julia Dickey <julia@>
2016-06-06 13:12:14 -0700

    update js to use looping, and create DRY code

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,29 @@
 $(document).ready(function() {
   $("#blanks form").submit(function(event) {
-    var person1Input = $("input#person1").val();
-    var person2Input = $("input#person2").val();
-    var placeInput = $("input#place").val();
-    var transportationInput = $("input#transportation").val();
-    var exclamationInput = $("input#exclamation").val();
-    var adjectiveInput = $("input#adjective").val();
-    var animalInput = $("input#animal").val();
+    var blanks = ["person1", "person2", "place", "transportation", "exclamation", "adjective", "animal"];
 
-    $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".place").text(placeInput);
-    $(".transportation").text(transportationInput);
-    $(".exclamation").text(exclamationInput);
-    $(".adjective").text(adjectiveInput);
-    $(".animal").text(animalInput);
+    blanks.forEach(function(blank) {
+      var userInput = $("input#" + blank).val();
+      $("." + blank).text(userInput);
+    });
+
+
+
+    // var person1Input = $("input#person1").val();
+    // var person2Input = $("input#person2").val();
+    // var placeInput = $("input#place").val();
+    // var transportationInput = $("input#transportation").val();
+    // var exclamationInput = $("input#exclamation").val();
+    // var adjectiveInput = $("input#adjective").val();
+    // var animalInput = $("input#animal").val();
+    //
+    // $(".person1").text(person1Input);
+    // $(".person2").text(person2Input);
+    // $(".place").text(placeInput);
+    // $(".transportation").text(transportationInput);
+    // $(".exclamation").text(exclamationInput);
+    // $(".adjective").text(adjectiveInput);
+    // $(".animal").text(animalInput);
 
     $("#story").show();
 
